89d2106635d982b15feb9c1533b77cf96a2df1d9
Vincent King <vincentking@vincents-macbook-pro.local>
2016-08-22 14:56:51 -0600

    change bg color and added constant

--- getDemoTH/ViewController.swift
+++ getDemoTH/ViewController.swift
@@ -11,6 +11,9 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+        
+        let userName = dummyUser
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
