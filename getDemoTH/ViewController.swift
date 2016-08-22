8302dc68ece9c3228a6703f0ef714bc647f6b4ac
Vincent King <vincentking@vincents-macbook-pro.local>
2016-08-22 14:57:32 -0600

    d

--- getDemoTH/ViewController.swift
+++ getDemoTH/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         
-        let userName = dummyUser
+        let userName = "Dummyuser"
         
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
